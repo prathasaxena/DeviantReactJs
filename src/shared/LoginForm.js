a995b4d5c2fd35ea1539d6b7903f445fbce62d73
shashank7099 <shashanksaxena799@gmail.com>
2020-08-28 22:58:35 +0530

    forms and css

--- src/shared/LoginForm.js
+++ src/shared/LoginForm.js
@@ -4,22 +4,29 @@ import { Form, Button } from 'react-bootstrap';
 
 const LoginForm = (props) => { 
     return (
-        <div>
-            <h1>Log in</h1>
-            <p>Become a deviant. <a>Join</a></p>
-     <Form>
-        <Form.Group controlId="formBasicEmail" className="form-group">
-        <Form.Control type="email" />
-        <Form.Label>Username</Form.Label>
-     </Form.Group>
-      <Form.Group controlId="formBasicPassword" className="form-group">
-          <Form.Control type="password" />
-          <Form.Label>Password</Form.Label>
-     </Form.Group>
-      <Button variant="primary" type="submit">
-      Submit
-      </Button>
-    </Form>
+    <div>
+        <h1>Log in</h1>
+        <p>Become a deviant. <a>Join</a></p>
+            <form >
+        <div className="form-group">
+            <input type="email" />
+            <label>Username</label>
+        </div>
+        <div  className="form-group">
+          <input type="password" />
+          <label>Password</label>
+        </div>
+        <div className="form-group-row">
+            <input
+                name="isGoing"
+                type="checkbox"
+                checked={true}/>
+            <label> Keep me logged in </label>       
+         </div>
+       <Button variant="primary" type="submit">
+          Log in
+        </Button>
+    </form>
     </div>
     )
 }
